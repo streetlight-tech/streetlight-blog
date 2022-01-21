40151582832300ceba408ae86479e941631b4237
Doug Lampe <lampedoug@yahoo.com>
2022-01-20 22:49:07 -0500

    fix: fix handler

--- dist/handler.js
+++
@@ -1,9 +0,0 @@
-exports.trigger = async (event) => {
-  return {
-    headers: {
-      "Content-Type": "text.html",
-    },
-    body: "<html><head><title>Hello World<title></head><body>Hello World</body></html>",
-    statusCode: 200,
-  };
-};
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+  return {
+    headers: {
+      "Content-Type": "text.html",
+    },
+    body: "<html><head><title>Hello World</title></head><body>Hello World</body></html>",
+    statusCode: 200,
+  };
+};
